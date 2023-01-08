c29e0cd613e1a23f37824781a31111ad95ecc6a8
Felipe Bambirra <lipex360@gmail.com>
2023-01-07 21:23:02 -0300

    project updated

---
+++ README.md
@@ -0,0 +1,55 @@
+<h2 align="center">SASS</h2>
+<p align="center">HTML + SCSS simple project with Github API consumer</p>
+
+---
+
+<p align="center">
+  <img src="https://github.com/lipex360x/sass/blob/main/assets/screen.png" />
+</p>
+
+---
+
+#### :bookmark_tabs: Content Index
+
+- [Introduction](#zap-introduction)
+
+- [Startup](#zap-startup)
+
+- [Technologies](#zap-technologies)
+
+- [Live Preview](https://vitets-sass.netlify.app/)
+
+---
+
+#### :zap: Introduction
+
+This is a simple HTML and SASS Project
+
+---
+
+#### :zap: Startup
+
+- Clone this project
+
+- Install dependencies with `yarn` or `npm install`
+
+- run `yarn dev` or `npm run dev`
+
+---
+
+
+#### :zap: Technologies
+
+```
+  typescript
+  axios
+  SASS
+```
+
+---
+
+:point_up_2: [Go to Content Index](#bookmark_tabs-content-index)
+
+---
+
+:pushpin: Tips: for easier navigation by github, consider installing the [Octotree](https://chrome.google.com/webstore/detail/octotree-github-code-tree/bkhaagjahfmjljalopjnoealnfndnagc) plugin
--- src/scripts/functions.ts
+++ src/scripts/functions.ts
@@ -1,11 +1,21 @@
 import { api } from "../core/client/axios";
 
+const getMain = document.querySelector(".main") as HTMLDivElement;
 const getForm = document.querySelector(".form");
 const getInput = document.querySelector(".input") as HTMLInputElement;
 const getHead = document.querySelector(".github-user") as HTMLHeadElement;
 
 interface GithubResponse {
   name: string;
+}
+
+function errorShake() {
+  getHead.innerHTML = `try again...`;
+  getMain.classList.add("error-shake");
+
+  setTimeout(() => {
+    getMain.classList.remove("error-shake");
+  }, 1000);
 }
 
 async function submitForm(): Promise<void> {
@@ -21,7 +31,7 @@ async function submitForm(): Promise<void> {
 
       getHead.innerHTML = `Hello ${name}`;
     } catch (error) {
-      getHead.innerHTML = `try again...`;
+      errorShake();
       console.error(error);
     } finally {
       getInput.value = "";
@@ -30,6 +40,7 @@ async function submitForm(): Promise<void> {
 }
 
 async function bootstrap(): Promise<void> {
+  getInput.focus();
   void submitForm();
 }
 void bootstrap();
